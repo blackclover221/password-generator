d58d0ab3db1753fc395acbf201e38ec9d777c9e8
blackclover221 <yasminismail33@gmail.com>
2023-08-29 23:47:53 +0100

    completed the code part of the password generator

--- starter/script.js
+++ starter/script.js
@@ -90,17 +90,114 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  let length = parseInt(
+    prompt("How many characters do you want your password to comprise?")
+  )
+
+
+  if (isNaN(length) === true) {
+    alert('it is mandotry you provide a pasword length')
+    return;
+  }
+
+  if (length < 8) {
+    alert('password is too short! password must length must be at least 8 characters');
+    return;
+  }
+
+  if (length >= 55) {
+    alert('password is too long! password length must not be more than 55 characters');
+    return;
+  }
+
+  let hasNumericCharacters = confirm(
+    "Click OK to confirm including number characters"
+  )
+
+  let hasSpecialCharacters = confirm(
+    "Click OK to confirm including special characters"
+  )
+  let hasLowerCaseCharacters = confirm(
+    "Click OK to confirm lowercase characters"
+  )
+
+  let hasUpperCaseCharacters = confirm(
+    "Click OK to confirm uppercase characters"
+  )
+
+  if (hasNumericCharacters === false && hasSpecialCharacters === false && hasLowerCaseCharacters === false && hasUpperCaseCharacters === false) {
+    alert('You must select at least one character type');
+    return;
+  }
+
+  let getPasswordOptions = {
+    length: length,
+    hasNumericCharacters: hasNumericCharacters,
+    hasSpecialCharacters: hasSpecialCharacters,
+    hasLowerCaseCharacters: hasLowerCaseCharacters,
+    hasUpperCaseCharacters: hasUpperCaseCharacters,
+
+  }
+
+  console.log(getPasswordOptions);
+
+  return getPasswordOptions;
 
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
+  let randomIndex = Math.floor(Math.random() * arr.length)
+  let randomElement = arr[randomIndex];
 
+  return randomElement;
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  let options = getPasswordOptions();
+  console.log(options);
+  let result = []
 
+
+  let possibleCharacter = []
+
+  let guaranteedCharacter = []
+
+  if (options.hasSpecialCharacters) {
+    possibleCharacter = possibleCharacter.concat(specialCharacters);
+    guaranteedCharacter.push(getRandom(specialCharacters))
+  }
+
+  if (options.hasNumericCharacters) {
+    possibleCharacter = possibleCharacter.concat(numericCharacters);
+    guaranteedCharacter.push(getRandom(numericCharacters))
+  }
+
+  if (options.hasLowerCaseCharacters) {
+    possibleCharacter = possibleCharacter.concat(lowerCasedCharacters);
+    guaranteedCharacter.push(getRandom(lowerCasedCharacters))
+  }
+
+  if (options.hasUpperCaseCharacters) {
+    possibleCharacter = possibleCharacter.concat(upperCasedCharacters);
+    guaranteedCharacter.push(getRandom(upperCasedCharacters))
+  }
+
+  console.log(possibleCharacter);
+
+  for (let i = 0; i < options.length; i++) {
+    var generated = getRandom(possibleCharacter);
+    console.log(generated);
+    result.push(generated);
+  }
+
+  for (let i = 0; i < guaranteedCharacter.length; i++) {
+    result[i] = guaranteedCharacter[i]
+  }
+  console.log(result);
+
+  return result.join("")
 }
 
 // Get references to the #generate element
